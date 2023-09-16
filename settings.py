4f264a88c3c992188de3472161bf0166ee9a33f3
Matheus Monteiro <math.a.monteiro@gmail.com>
2023-09-16 15:33:35 -0300

    Instruções no arquivo de configuração

--- settings.py
+++ settings.py
@@ -12,7 +12,38 @@ case6: (4,7,5,9,2,1,3,6,8) - 27 passos - em sala
 case7: (6, 7, 5, 1, 2, 3, 9, 4, 8) - 18 passos  - slide
 case8: (3, 1, 8, 5, 6, 2, 7, 4, 9) - 24 passos - slide
 """
-  
+
+"""
+
+- Como utilizar:
+
+  Este é o arquivo de configuração do programa. Ele permite definir dois parâmetros para a execução: o tipo de heurística utilizado e o estado inicial.
+
+  A definição dos parâmetros deve observar as seguintes regras:
+
+  - INITIAL_STATE:
+
+    - Informa um estado inicial para a execução do programa (ou seja, qual a configuração inicial do tabuleiro do 8-puzzle)
+    - Deve ser definido como uma tupla com 9 elementos, a qual é "enxergada" pelo programa como uma matriz 3x3. Por exemplo, a tupla (7,8,4,5,6,1,9,3,2)
+      representa o tabuleiro:
+
+          7 8 4
+          5 6 1
+            3 2
+
+    - Cada elemento deve ser um número inteiro entre 1 e 9
+    - Conforme exemplo, os inteiros informados na tupla representam o número do "bloco" no tabuleiro do jogo, com exceção do número 9, que representa o bloco vazio.
+    - Não é realizada nenhuma validação da configuração informada, cabendo ao usuário certificar-se de que inseriu uma configuração válida.
+
+  - HEURISTIC:
+
+    - Informa ao programa qual heurística deve ser utilizada, conforme a legenda:
+
+      - 0 (ou qualquer número diferente de 1 e 2): Não utiliza heurística
+      - 1: Utiliza heurística simples
+      - 2: Utiliza heurística mais precisa (avançada) 
+
+"""
   
 SETTINGS = {
   "HEURISTIC": 2,
